63a8aef67b24514504d785d30dd992c5d5bc3ac2
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-23 15:34:56 +0100

    minimal changes

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -5,15 +5,34 @@ public class Main {
     public static void main(String[] args) {
         Simulation verseny = new Simulation(Integer.parseInt(args[0]));
 
-        verseny.horsesFromFile("Horses.csv");
-        for (int i = 0;i < verseny.getHorses().length;i++) {
+        for(int i = 0; i < verseny.getAmountOfSimulations(); i++) {
+            verseny.horsesFromFile("Horses.csv");
+            for (int j = 0; j < verseny.getHorses().length; j++) {
+                //System.out.println(verseny.getHorses()[i].getSpeed());
+                //verseny.countSpeed(verseny.getHorses()[i]);
+                //System.out.println(verseny.getHorses()[i].getSpeed());
+                //System.out.println("countlaptime: " + verseny.countLapTime(verseny.getHorses()[i]));
+                //System.out.println("countrip: " + verseny.tripCount(verseny.getHorses()[i]));
+                //System.out.println((verseny.getHorses()[i].getName()) + "    " + verseny.countLapTime(verseny.getHorses()[i]));
+                verseny.countLapTime(verseny.getHorses()[j]);
+                System.out.println(verseny.getHorses()[j]);
+                System.out.println("\n\n");
+            }
+        }
+
+        verseny.generateData();
+
+        verseny.load();
+
+        for (int j = 0; j < verseny.getLoadHorses().length; j++) {
             //System.out.println(verseny.getHorses()[i].getSpeed());
             //verseny.countSpeed(verseny.getHorses()[i]);
             //System.out.println(verseny.getHorses()[i].getSpeed());
-            System.out.println("countlaptime: " + verseny.countLapTime(verseny.getHorses()[i]));
-            System.out.println("countrip: " + verseny.tripCount(verseny.getHorses()[i]));
-            System.out.println((verseny.getHorses()[i].getName()) + "    " + verseny.countLapTime(verseny.getHorses()[i]));
+            //System.out.println("countlaptime: " + verseny.countLapTime(verseny.getHorses()[i]));
+            //System.out.println("countrip: " + verseny.tripCount(verseny.getHorses()[i]));
+            //System.out.println((verseny.getHorses()[i].getName()) + "    " + verseny.countLapTime(verseny.getHorses()[i]));
+            System.out.println(verseny.getLoadHorses()[j]);
             System.out.println("\n\n");
-        }
+            }
     }
 }
