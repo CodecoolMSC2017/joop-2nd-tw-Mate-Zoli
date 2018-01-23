7d1d0b019d1289d34ac85b638d4c6c91082d24df
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-23 15:34:34 +0100

    constructor is modified, lapTime fild is added, toString() method is added

--- src/com/codecool/Horse.java
+++ src/com/codecool/Horse.java
@@ -8,14 +8,26 @@ public class Horse {
     private int jockeyWeigth;
     private int tripChance;
     private int energyLevel;
+    private double lapTime;
 
     public Horse(String name, String jockeyName, int speed, int jockeyWeigth, int tripChance,int energyLevel) {
         this.name = name;
-        this.speed = speed;
         this.jockeyName = jockeyName;
+        this.speed = speed;
         this.jockeyWeigth = jockeyWeigth;
         this.tripChance = tripChance;
         this.energyLevel = energyLevel;
+        this.lapTime = 0;
+    }
+
+    public Horse(String name, String jockeyName, int speed, int jockeyWeigth, int tripChance,int energyLevel, double lapTime) {
+        this.name = name;
+        this.jockeyName = jockeyName;
+        this.speed = speed;
+        this.jockeyWeigth = jockeyWeigth;
+        this.tripChance = tripChance;
+        this.energyLevel = energyLevel;
+        this.lapTime = lapTime;
     }
 
     public String getName() {
@@ -42,12 +54,31 @@ public class Horse {
         jockeyWeigth += weightChange;
     }
 
-    public double getTripChance() {
+    public int getTripChance() {
         return tripChance;
     }
 
-    public double getEnergyLevel() {
+    public int getEnergyLevel() {
         return energyLevel;
+    }
+
+    public double getLapTime() {
+        return lapTime;
+    }
+
+    public void setLapTime(double lapTime) {
+        this.lapTime = lapTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Horse's name: " + name 
+                + ", Horse's speed: " + speed
+                + ", Jockey's name: " + jockeyName
+                + ", Jockey's weigth: " + jockeyWeigth
+                + ", Trip chance: " + tripChance
+                + ", Horse's energylevel: " + energyLevel
+                + ", Horse's lap time: " + lapTime;
     }
 
 }
