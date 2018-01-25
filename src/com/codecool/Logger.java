29a5c6f7bc5a89f6c92d57aaa540e7b1120861e6
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-25 11:12:08 +0100

    printTime() method is modified

--- src/com/codecool/Logger.java
+++ src/com/codecool/Logger.java
@@ -13,10 +13,10 @@ public class Logger {
     
     public String printTime(double horseLaptTime)  {
         String startTime = "00:00:00";
-        double seconds = horseLaptTime*60;
-        int m = (int)Math.round(seconds / 3600 + Integer.parseInt(startTime.substring(0,1)));
-        int s = (int)Math.round((seconds % 3600)/60 + Integer.parseInt(startTime.substring(3,4)));
-        int ms = (int)Math.round((seconds % 60) + Integer.parseInt(startTime.substring(6,7)));
+        double seconds = horseLaptTime*1000;
+        int m = (int)Math.round(seconds / 60000 + Integer.parseInt(startTime.substring(0,1)));
+        int s = (int)Math.round((seconds % 60000)/1000 + Integer.parseInt(startTime.substring(3,4)));
+        int ms = (int)Math.round((seconds % 1000) + Integer.parseInt(startTime.substring(6,7)));
         String newtime = m+":"+s+":"+ms;
         return newtime;
     }
